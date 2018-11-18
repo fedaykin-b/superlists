a793b11d0bde901d704c4a83d9330eed15ab5d1d
Bruno Arruda <arrudabruno10@gmail.com>
2018-11-18 21:24:54 -0200

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,6 +8,7 @@ from django.test import LiveServerTestCase
 
 MAX_WAIT = 10
 
+
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -15,7 +16,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-    
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -53,7 +54,7 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1. Buy peacock feathers')
-        
+
         # There is still a text box inviting her t add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -63,9 +64,9 @@ class NewVisitorTest(LiveServerTestCase):
         # The pages updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1. Buy peacock feathers')
         self.wait_for_row_in_list_table('2. Use peacock feathers to make a fly')
-        
+
         # Satisfied, she goes back to sleep
-    
+
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new list
         self.browser.get(self.live_server_url)
@@ -104,6 +105,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
 
 from lists.models import Item
+
 
 class SmokeTest(TestCase):
 
@@ -15,15 +14,17 @@ class SmokeTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={"item_text":'A new list item'})
+        self.client.post('/', data={"item_text": 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={"item_text":'A new list item'})
+        response = self.client.post('/', data={"item_text": 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'],
+                         '/lists/the-only-list-in-the-world/')
+
 
 class ItemModelTest(TestCase):
 
@@ -42,13 +43,22 @@ class ItemModelTest(TestCase):
         self.assertEqual(saved_items[0].text, 'The first (ever) list item')
         self.assertEqual(saved_items[1].text, 'Item the second')
 
+
 class HomePageTest(TestCase):
+    pass
+
+
+class ListViewTest(TestCase):
 
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
