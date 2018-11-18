d93f51108c21524ca49bae3822310fe887699cc0
Bruno Arruda <arrudabruno10@gmail.com>
2018-11-18 18:40:38 -0200

    New FT to test multiple users with exclusive lists

--- lists/tests.py
+++ lists/tests.py
@@ -9,11 +9,11 @@ class SmokeTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-    
+
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-    
+
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={"item_text":'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
