f2732fac1b3190a4b5be597e9d788c7f1b7974a7
Bruno Arruda <arrudabruno10@gmail.com>
2018-11-18 22:20:04 -0200

    adde a model to support multiple lists, and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 class SmokeTest(TestCase):
@@ -23,21 +23,30 @@ class NewListTest(TestCase):
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
-
+        self.assertEqual(saved_items[0].list, list_)
         self.assertEqual(saved_items[0].text, 'The first (ever) list item')
+        self.assertEqual(saved_items[1].list, list_)
         self.assertEqual(saved_items[1].text, 'Item the second')
 
 
@@ -48,8 +57,9 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item 1', list=list_)
+        Item.objects.create(text='item 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
