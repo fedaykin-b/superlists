2ab3615ef3503c2e9f1329d2e6a62d22c69d013f
Bruno Arruda <arrudabruno10@gmail.com>
2018-11-19 11:04:31 -0200

    removed unused variables in list/views and tests

--- lists/views.py
+++ lists/views.py
@@ -9,7 +9,6 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'list': list_})
 
 
@@ -21,5 +20,5 @@ def new_list(request):
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
